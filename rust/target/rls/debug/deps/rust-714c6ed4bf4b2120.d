18c10585ca91e79559c004107ad197e083c7446e
AmdaUwU <jbat2000@gmail.com>
2022-06-14 20:23:05 -0400

    rust stuff

--- rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.d
+++ rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.d
@@ -1,6 +1,6 @@
-/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.rmeta: src/main.rs src/a4.rs
+/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.rmeta: src/main.rs src/a5.rs
 
-/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.d: src/main.rs src/a4.rs
+/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-714c6ed4bf4b2120.d: src/main.rs src/a5.rs
 
 src/main.rs:
-src/a4.rs:
+src/a5.rs:
--- rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.d
+++ rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.d
@@ -1,6 +1,6 @@
-/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.rmeta: src/main.rs src/a4.rs
+/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.rmeta: src/main.rs src/a5.rs
 
-/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.d: src/main.rs src/a4.rs
+/home/amda/Documents/Projects/Project_Euler/rust/target/rls/debug/deps/rust-a2281fa4dacafbe9.d: src/main.rs src/a5.rs
 
 src/main.rs:
-src/a4.rs:
+src/a5.rs:
